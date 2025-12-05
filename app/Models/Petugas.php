6be68ffea9d78fce2f459445974db4c2c61bcbbb
aryarrayra <aryarrayra1@gmail.com>
2025-12-05 09:33:22 +0700

    Models

--- app/Models/Petugas.php
+++ app/Models/Petugas.php
@@ -43,4 +43,29 @@ class Petugas extends Model
     {
         return $this->hasMany(JadwalSewa::class, 'id_petugas');
     }
+
+        public function checkPassword($inputPassword)
+    {
+        // Log untuk debugging
+        \Log::info('Checking password:', [
+            'input' => $inputPassword,
+            'stored' => $this->password,
+            'stored_length' => strlen($this->password)
+        ]);
+        
+        // 1. Jika password di DB adalah plain text
+        if ($this->password === $inputPassword) {
+            \Log::info('Password match (plain text)');
+            return true;
+        }
+        
+        // 2. Jika di DB sudah di-hash (fallback untuk legacy)
+        if (Hash::check($inputPassword, $this->password)) {
+            \Log::info('Password match (hashed)');
+            return true;
+        }
+        
+        \Log::warning('Password tidak cocok');
+        return false;
+    }
 }
